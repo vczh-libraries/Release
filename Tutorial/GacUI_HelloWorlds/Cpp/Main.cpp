17659534e15c11b36381f395f8cadfb991cd60ba
vczh <vczh@163.com>
2015-09-26 13:02:08 -0700

    Update Tutorial

---
+++ Tutorial/GacUI_HelloWorlds/Cpp/Main.cpp
@@ -0,0 +1,29 @@
+#include <GacUI.h>
+#include <Windows.h>
+
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int CmdShow)
+{
+	return SetupWindowsDirect2DRenderer();
+}
+
+void GuiMain()
+{
+	GuiWindow* window = g::NewWindow();
+	window->SetText(L"Hello, world!");
+	window->SetClientSize(Size(480, 320));
+	window->GetBoundsComposition()->SetPreferredMinSize(Size(480, 320));
+	window->MoveToScreenCenter();
+
+	GuiControl* label = g::NewLabel();
+	label->SetText(L"Welcome to GacUI Library!");
+	{
+		FontProperties font;
+		font.fontFamily = L"Segoe UI";
+		font.size = 32;
+		font.antialias = true;
+		label->SetFont(font);
+	}
+	window->AddChild(label);
+	GetApplication()->Run(window);
+	delete window;
+}
---
+++ Tutorial/GacUI_HelloWorlds/CppXml/Main.cpp
@@ -0,0 +1,23 @@
+#include "UI/Source/HelloWorld.h"
+#include <Windows.h>
+
+using namespace vl::collections;
+using namespace vl::stream;
+
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int CmdShow)
+{
+	return SetupWindowsDirect2DRenderer();
+}
+
+void GuiMain()
+{
+	{
+		List<WString> errors;
+		FileStream fileStream(L"../UIRes/CppXml.bin", FileStream::ReadOnly);
+		auto resource = GuiResource::LoadPrecompiledBinary(fileStream, errors);
+		GetInstanceLoaderManager()->SetResource(L"Resource", resource);
+	}
+	helloworld::MainWindow window;
+	window.MoveToScreenCenter();
+	GetApplication()->Run(&window);
+}
