2049c4116b5faeb62606eab403d8ec65e64b28c9
vczh <vczh@163.com>
2015-09-26 13:25:59 -0700

    MVVM Tutorial

---
+++ Tutorial/GacUI_HelloWorlds/CppXml/WinMain.cpp
@@ -0,0 +1,7 @@
+#include <GacUI.h>
+#include <Windows.h>
+
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int CmdShow)
+{
+	return SetupWindowsDirect2DRenderer();
+}
--- Tutorial/GacUI_HelloWorlds/MVVM/Main.cpp
+++ Tutorial/GacUI_HelloWorlds/MVVM/Main.cpp
@@ -1,11 +1,88 @@
-#include <GacUI.h>
-#include <Windows.h>
+#include "UI/Source/HelloWorld.h"
 
-int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int CmdShow)
+using namespace vl::collections;
+using namespace vl::stream;
+using namespace vl::regex;
+
+class ViewModel : public Object, public virtual vm::IViewModel
 {
-	return SetupWindowsDirect2DRenderer();
-}
+private:
+	WString userName;
+	WString password;
+	Regex regexLcLetters;
+	Regex regexUcLetters;
+	Regex regexNumbers;
+
+public:
+	ViewModel()
+		:regexLcLetters(L"[a-z]")
+		, regexUcLetters(L"[A-Z]")
+		, regexNumbers(L"[0-9]")
+	{
+	}
+
+	WString GetUserName()override
+	{
+		return userName;
+	}
+
+	void SetUserName(WString value)override
+	{
+		userName = value;
+		UserNameErrorChanged();
+	}
+
+	WString GetUserNameError()override
+	{
+		if (userName == L"")
+		{
+			return L"User name should not be empty.";
+		}
+		return L"";
+	}
+
+	WString GetPassword()override
+	{
+		return password;
+	}
+
+	void SetPassword(WString value)override
+	{
+		password = value;
+		PasswordErrorChanged();
+	}
+
+	WString GetPasswordError()override
+	{
+		if (password == L"")
+		{
+			return L"Password should not be empty.";
+		}
+		bool containsLowerCases = regexLcLetters.Match(password);
+		bool containsUpperCases = regexUcLetters.Match(password);
+		bool containsNumbers = regexNumbers.Match(password);
+		if (!containsLowerCases || !containsUpperCases || !containsNumbers)
+		{
+			return L"Password should contains at least one lower case letter, one upper case letter and one digit.";
+		}
+		return L"";
+	}
+
+	bool SignUp()override
+	{
+		return true;
+	}
+};
 
 void GuiMain()
 {
+	{
+		List<WString> errors;
+		FileStream fileStream(L"../UIRes/MVVM.bin", FileStream::ReadOnly);
+		auto resource = GuiResource::LoadPrecompiledBinary(fileStream, errors);
+		GetInstanceLoaderManager()->SetResource(L"Resource", resource);
+	}
+	helloworld::MainWindow window(new ViewModel);
+	window.MoveToScreenCenter();
+	GetApplication()->Run(&window);
 }
