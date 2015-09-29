ee954f7ff0f6b08273f6af738c0ce11e06a5b883
vczh <vczh@163.com>
2015-09-28 22:31:10 -0700

    New empty tutorial projects

--- Tutorial/Codegen.bat
+++ Tutorial/Codegen.bat
@@ -6,3 +6,20 @@ pushd MVVM\UI
 call Codegen.bat
 popd
 popd
+pushd GacUI_Layout
+pushd Alignment\UI
+call Codegen.bat
+popd
+pushd Stack\UI
+call Codegen.bat
+popd
+pushd Table\UI
+call Codegen.bat
+popd
+pushd Flow\UI
+call Codegen.bat
+popd
+pushd RichTextEmbedding\UI
+call Codegen.bat
+popd
+popd
