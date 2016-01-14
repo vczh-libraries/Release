9de8de3d9fed3e7d80affd17e6f28fc0bb224c3e
vczh <vczh@163.com>
2016-01-13 20:16:30 -0800

    ...

---
+++ Tutorial/GacUI_ControlTemplate/BlackSkin/Main.cpp
@@ -0,0 +1,25 @@
+#include "UI/Source/Demo.h"
+#include <Windows.h>
+
+using namespace vl::collections;
+using namespace vl::stream;
+
+#pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int CmdShow)
+{
+	return SetupWindowsDirect2DRenderer();
+}
+
+void GuiMain()
+{
+	{
+		List<WString> errors;
+		FileStream fileStream(L"../UIRes/TextEditor.bin", FileStream::ReadOnly);
+		auto resource = GuiResource::LoadPrecompiledBinary(fileStream, errors);
+		GetInstanceLoaderManager()->SetResource(L"Resource", resource);
+	}
+	demo::MainWindow window;
+	window.MoveToScreenCenter();
+	GetApplication()->Run(&window);
+}
