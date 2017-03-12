6dd8b3192e6dff666eb22328ac7f1fb6071246f4
Zihan Chen <vczh@163.com>
2017-03-11 23:52:05 -0800

    Upgrade Tutorial\GacUI_Layout

--- Tutorial/GacUI_Layout/RichTextEmbedding/Main.cpp
+++ Tutorial/GacUI_Layout/RichTextEmbedding/Main.cpp
@@ -22,9 +22,8 @@ public:
 void GuiMain()
 {
 	{
-		List<WString> errors;
 		FileStream fileStream(L"../UIRes/RichTextEmbedding.bin", FileStream::ReadOnly);
-		auto resource = GuiResource::LoadPrecompiledBinary(fileStream, errors);
+		auto resource = GuiResource::LoadPrecompiledBinary(fileStream);
 		GetResourceManager()->SetResource(L"Resource", resource);
 	}
 	demo::MainWindow window(new ViewModel);
--- Tutorial/GacUI_Layout/TableSplitter/Main.cpp
+++ Tutorial/GacUI_Layout/TableSplitter/Main.cpp
@@ -13,9 +13,8 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 void GuiMain()
 {
 	{
-		List<WString> errors;
 		FileStream fileStream(L"../UIRes/TableSplitter.bin", FileStream::ReadOnly);
-		auto resource = GuiResource::LoadPrecompiledBinary(fileStream, errors);
+		auto resource = GuiResource::LoadPrecompiledBinary(fileStream);
 		GetResourceManager()->SetResource(L"Resource", resource);
 	}
 	demo::MainWindow window;
