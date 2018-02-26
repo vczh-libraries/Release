c22c68c649dffe3dadc647a85b50918a34570285
Zihan Chen <vczh@163.com>
2018-02-26 03:38:01 -0800

    Demo: GacUI_Controls\MenuVisibility

---
+++ Tutorial/GacUI_Controls/MenuVisibility/Main.cpp
@@ -0,0 +1,19 @@
+#define GAC_HEADER_USE_NAMESPACE
+#include "UI/Source/Demo.h"
+
+using namespace vl::collections;
+using namespace vl::stream;
+using namespace vl::reflection::description;
+using namespace demo;
+
+void GuiMain()
+{
+	{
+		FileStream fileStream(L"../UIRes/MenuVisibility.bin", FileStream::ReadOnly);
+		auto resource = GuiResource::LoadPrecompiledBinary(fileStream);
+		GetResourceManager()->SetResource(L"Resource", resource);
+	}
+	demo::MainWindow window;
+	window.MoveToScreenCenter();
+	GetApplication()->Run(&window);
+}
