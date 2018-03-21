29309581aa51c715e8da3989e5a990646e79b964
Zihan Chen <vczh@163.com>
2018-03-21 02:59:50 -0700

    demo: GacUI_Windows\Direct2DClock

---
+++ Tutorial/GacUI_Windows/Direct2DClock/Main.cpp
@@ -0,0 +1,19 @@
+#define GAC_HEADER_USE_NAMESPACE
+#include "UI/Source/Demo.h"
+
+using namespace vl::collections;
+using namespace vl::stream;
+using namespace vl::reflection::description;
+using namespace demo;
+
+void GuiMain()
+{
+	{
+		FileStream fileStream(L"../UIRes/Direct2DClock.bin", FileStream::ReadOnly);
+		auto resource = GuiResource::LoadPrecompiledBinary(fileStream);
+		GetResourceManager()->SetResource(L"Resource", resource);
+	}
+	MainWindow window;
+	window.MoveToScreenCenter();
+	GetApplication()->Run(&window);
+}
