a67fc2929acfceb7085b1db84c3e88947f559a51
Zihan Chen <vczh@163.com>
2019-01-16 02:46:30 -0800

    ...

--- Tutorial/GacUI_Windows/WindowIcon/Main.cpp
+++ Tutorial/GacUI_Windows/WindowIcon/Main.cpp
@@ -8,9 +8,22 @@ using namespace vl::stream;
 using namespace vl::reflection::description;
 using namespace demo;
 
+/*
+The code has to run before calling SetupWindowsDirect2DRenderer
+But I am lazy and I don't want to create another GacUILite without WinMain
+So I just do this
+*/
+class Initialize
+{
+public:
+	Initialize()
+	{
+		windows::SetWindowDefaultIcon(MAINICON);
+	}
+} initialize;
+
 void GuiMain()
 {
-	windows::SetWindowDefaultIcon(MAINICON);
 	{
 		FileStream fileStream(L"../UIRes/WindowIcon.bin", FileStream::ReadOnly);
 		GetResourceManager()->LoadResourceOrPending(fileStream);
