ccf6ba21d21fe021d37585cc71ae15550074b9e3
Zihan Chen <vczh@163.com>
2020-07-31 18:37:08 -0700

    ...

---
+++ Tutorial/Console_Workflow/W06_Debugging/Main.cpp
@@ -0,0 +1,151 @@
+#include <VlppWorkflowCompiler.h>
+
+using namespace vl;
+using namespace vl::console;
+using namespace vl::collections;
+using namespace vl::parsing;
+using namespace vl::reflection;
+using namespace vl::reflection::description;
+using namespace vl::workflow;
+using namespace vl::workflow::emitter;
+using namespace vl::workflow::runtime;
+
+namespace myapi
+{
+	class App : public Object, public Description<App>
+	{
+	public:
+		static WString Get()
+		{
+			return Console::Read();
+		}
+
+		static WString Get(const WString& message)
+		{
+			Console::Write(message);
+			return Console::Read();
+		}
+
+		static void Print(const WString& text)
+		{
+			Console::WriteLine(text);
+		}
+	};
+}
+
+#define MYAPI_TYPELIST(F)\
+	F(myapi::App)\
+
+namespace vl
+{
+	namespace reflection
+	{
+		namespace description
+		{
+			MYAPI_TYPELIST(DECL_TYPE_INFO)
+			MYAPI_TYPELIST(IMPL_CPP_TYPE_INFO)
+
+			using namespace myapi;
+
+#define _ ,
+
+			BEGIN_CLASS_MEMBER(App)
+				CLASS_MEMBER_STATIC_METHOD_OVERLOAD(Get, NO_PARAMETER, WString(*)())
+				CLASS_MEMBER_STATIC_METHOD_OVERLOAD(Get, { L"message" }, WString(*)(const WString&))
+				CLASS_MEMBER_STATIC_METHOD(Print, { L"text" })
+			END_CLASS_MEMBER(App)
+
+#undef _
+			class MyApiTypeLoader : public Object, public ITypeLoader
+			{
+			public:
+				void Load(ITypeManager* manager)
+				{
+					MYAPI_TYPELIST(ADD_TYPE_INFO)
+				}
+
+				void Unload(ITypeManager* manager)
+				{
+				}
+			};
+		}
+	}
+}
+
+const wchar_t ScriptCode[] = LR"Workflow(
+
+module sampleModule;
+
+using myapi::*;
+
+func main(): void
+{
+	var name = App::Get("Please enter your name:");
+	App::Print($"Hello, $(name)!");
+}
+
+)Workflow";
+
+class MyDebugger : public WfDebugger
+{
+protected:
+	void OnBlockExecution()
+	{
+		// get the context for the current thread
+		auto context = GetCurrentThreadContext();
+
+		// whatever the input is, change the "name" variable
+		auto assembly = context->globalContext->assembly;
+		vint functionIndex = context->GetCurrentStackFrame().functionIndex;
+		auto functionMetadata = assembly->functions[functionIndex];
+		vint indexName = functionMetadata->localVariableNames.IndexOf(L"name");
+		context->StoreLocalVariable(indexName, BoxValue(WString(L"MyDebugger")));
+
+		// continue
+		Run();
+	}
+};
+
+int main()
+{
+	// start reflection
+	LoadPredefinedTypes();
+	WfLoadLibraryTypes();
+	GetGlobalTypeManager()->AddTypeLoader(new MyApiTypeLoader);
+	GetGlobalTypeManager()->Load();
+
+	{
+		// prepare Workflow script code
+		List<WString> codes;
+		codes.Add(WString(ScriptCode, false));
+
+		// compile code and get assemblies
+		List<Ptr<ParsingError>> errors;
+		auto table = WfLoadTable();
+		auto assembly = Compile(table, codes, errors);
+		CHECK_ERROR(assembly && errors.Count() == 0, L"Please check the 'errors' variable.");
+
+		// initialize the assembly
+		auto globalContext = MakePtr<WfRuntimeGlobalContext>(assembly);
+		auto initializeFunction = LoadFunction<void()>(globalContext, L"<initialize>");
+		initializeFunction();
+
+		// start debugging
+		auto debugger = MakePtr<WfDebugger>();
+		SetDebuggerForCurrentThread(debugger);
+
+		// break at App::Print(...);
+		// it is in file 0 row 9
+		debugger->AddCodeLineBreakPoint(assembly.Obj(), 0, 9, true);
+
+		// call main
+		auto mainFunction = LoadFunction<void()>(globalContext, L"main");
+		mainFunction();
+
+		// stop debugging
+		SetDebuggerForCurrentThread(nullptr);
+	}
+
+	// stop reflection
+	DestroyGlobalTypeManager();
+}
