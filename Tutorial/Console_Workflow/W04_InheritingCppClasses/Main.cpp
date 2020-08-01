1e8616e5ebd4450cd76273267a17a61ed1882355
Zihan Chen <vczh@163.com>
2020-07-31 17:07:03 -0700

    ...

---
+++ Tutorial/Console_Workflow/W04_InheritingCppClasses/Main.cpp
@@ -0,0 +1,179 @@
+#include <VlppWorkflowCompiler.h>
+
+using namespace vl;
+using namespace vl::console;
+using namespace vl::collections;
+using namespace vl::parsing;
+using namespace vl::reflection;
+using namespace vl::reflection::description;
+using namespace vl::workflow;
+using namespace vl::workflow::emitter;
+using namespace vl::workflow::runtime;
+
+namespace myapi
+{
+	class App : public Object, public Description<Console>
+	{
+	private:
+		WString name;
+		List<Ptr<IValueSubscription>> subscriptions;
+
+	public:
+		static WString Get()
+		{
+			return Console::Read();
+		}
+
+		static WString Get(const WString& message)
+		{
+			Console::Write(message);
+			return Console::Read();
+		}
+
+		static void Print(const WString& text)
+		{
+			Console::WriteLine(text);
+		}
+
+		App() = default;
+
+		~App()
+		{
+			FOREACH(Ptr<IValueSubscription>, sub, subscriptions)
+			{
+				sub->Close();
+			}
+		}
+
+		Event<void()> NameChanged;
+
+		WString GetName()
+		{
+			return name;
+		}
+
+		void SetName(const WString& value)
+		{
+			if (name != value)
+			{
+				name = value;
+				NameChanged();
+			}
+		}
+
+		List<Ptr<IValueSubscription>>& GetSubscriptions()
+		{
+			return subscriptions;
+		}
+	};
+}
+
+#define MYAPI_TYPELIST(F)\
+	F(myapi::App)\
+
+namespace vl
+{
+	namespace reflection
+	{
+		namespace description
+		{
+			MYAPI_TYPELIST(DECL_TYPE_INFO)
+			MYAPI_TYPELIST(IMPL_CPP_TYPE_INFO)
+
+			using namespace myapi;
+
+#define _ ,
+
+			BEGIN_CLASS_MEMBER(App)
+				CLASS_MEMBER_STATIC_METHOD_OVERLOAD(Get, NO_PARAMETER, WString(*)())
+				CLASS_MEMBER_STATIC_METHOD_OVERLOAD(Get, { L"message" }, WString(*)(const WString&))
+				CLASS_MEMBER_STATIC_METHOD(Print, { L"text" })
+
+				CLASS_MEMBER_CONSTRUCTOR(Ptr<App>(), NO_PARAMETER)
+
+				CLASS_MEMBER_EVENT(NameChanged)
+				CLASS_MEMBER_PROPERTY_EVENT_FAST(Name, NameChanged)
+				CLASS_MEMBER_PROPERTY_READONLY_FAST(Subscriptions)
+			END_CLASS_MEMBER(App)
+
+#undef _
+			class MyApiTypeLoader : public Object, public ITypeLoader
+			{
+			public:
+				void Load(ITypeManager* manager)
+				{
+					MYAPI_TYPELIST(ADD_TYPE_INFO)
+				}
+
+				void Unload(ITypeManager* manager)
+				{
+				}
+			};
+		}
+	}
+}
+
+const wchar_t ScriptCode[] = LR"Workflow(
+
+module sampleModule;
+
+using myapi::*;
+
+namespace myscript
+{
+	class MyApp : App
+	{
+		new(name: string)
+		{
+			Name = name;
+
+			value subscription = bind($"Hello, $(this.Name)");
+			subscription.Open();
+			attach(subscription.ValueChanged,
+				func (value: object): void
+				{
+					Print(cast string value);
+				});
+			Subscriptions.Add(subscription);
+		}
+	}
+}
+
+)Workflow";
+
+int main()
+{
+	// start reflection
+	LoadPredefinedTypes();
+	WfLoadLibraryTypes();
+	GetGlobalTypeManager()->AddTypeLoader(new MyApiTypeLoader);
+	GetGlobalTypeManager()->Load();
+
+	{
+		// prepare Workflow script code
+		List<WString> codes;
+		codes.Add(WString(ScriptCode, false));
+
+		// compile code and get assemblies
+		List<Ptr<ParsingError>> errors;
+		auto table = WfLoadTable();
+		auto assembly = Compile(table, codes, errors);
+		CHECK_ERROR(assembly && errors.Count() == 0, L"Please check the 'errors' variable.");
+
+		// initialize the assembly
+		auto globalContext = MakePtr<WfRuntimeGlobalContext>(assembly);
+		auto initializeFunction = LoadFunction<void()>(globalContext, L"<initialize>");
+		initializeFunction();
+
+		// call MyApp
+		auto myapp = UnboxValue<Ptr<App>>(Value::Create(
+			L"myscript::MyApp",
+			(
+				Value_xs(), WString(L"vczh")
+			)));
+		myapp->SetName(L"Gaclib");
+	}
+
+	// stop reflection
+	DestroyGlobalTypeManager();
+}
