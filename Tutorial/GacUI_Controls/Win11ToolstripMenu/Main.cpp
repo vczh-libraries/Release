f2f4b32a60ea3b5cd3df4dd4123b1ec99f860327
vczh <vczh@outlook.com>
2021-10-15 03:15:57 -0700

    New tutorial: Win11ToolstripMenu

---
+++ Tutorial/GacUI_Controls/Win11ToolstripMenu/Main.cpp
@@ -0,0 +1,17 @@
+#define GAC_HEADER_USE_NAMESPACE
+#include "UI/Source/Demo.h"
+#include <Windows.h>
+
+using namespace vl::collections;
+using namespace vl::stream;
+
+void GuiMain()
+{
+	{
+		FileStream fileStream(L"../UIRes/Win11ToolstripMenu.bin", FileStream::ReadOnly);
+		GetResourceManager()->LoadResourceOrPending(fileStream);
+	}
+	demo::MainWindow window;
+	window.MoveToScreenCenter();
+	GetApplication()->Run(&window);
+}
