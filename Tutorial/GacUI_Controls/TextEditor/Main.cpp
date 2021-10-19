0e3446a22f4b1f2338905fd36d70963f3f2d6c00
vczh <vczh@outlook.com>
2021-10-19 02:42:30 -0700

    Remove Windows.h in tutorials

--- Tutorial/GacUI_Controls/TextEditor/Main.cpp
+++ Tutorial/GacUI_Controls/TextEditor/Main.cpp
@@ -1,6 +1,8 @@
 #define GAC_HEADER_USE_NAMESPACE
 #include "UI/Source/Demo.h"
+#ifdef VCZH_MSVC
 #include <Windows.h>
+#endif
 
 using namespace vl::collections;
 using namespace vl::stream;
@@ -24,7 +26,9 @@ OpenUrl
 
 void OpenUrl(const WString& url)
 {
+#ifdef VCZH_MSVC
 	ShellExecute(NULL, L"OPEN", url.Buffer(), NULL, NULL, SW_MAXIMIZE);
+#endif
 }
 
 /***********************************************************************
