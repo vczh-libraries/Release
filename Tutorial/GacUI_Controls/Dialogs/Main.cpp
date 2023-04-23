e8f9695994fde7eccdc92c7a1d8348f0391c0ea7
vczh <vczh@outlook.com>
2023-04-23 00:45:58 -0700

    Add tutorial: GacUI_Controls/Dialogs

---
+++ Tutorial/GacUI_Controls/Dialogs/Main.cpp
@@ -0,0 +1,16 @@
+#define GAC_HEADER_USE_NAMESPACE
+#include "UI/Source/Demo.h"
+
+using namespace vl::collections;
+using namespace vl::stream;
+
+void GuiMain()
+{
+	{
+		FileStream fileStream(L"../UIRes/Dialogs.bin", FileStream::ReadOnly);
+		GetResourceManager()->LoadResourceOrPending(fileStream);
+	}
+	demo::MainWindow window;
+	window.MoveToScreenCenter();
+	GetApplication()->Run(&window);
+}
